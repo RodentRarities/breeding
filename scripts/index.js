93a4563d0a8a0a1d5cd4665113c0bdffcb347942
root <root@localhost.localdomain>
2024-08-27 12:06:12 -0600

    update

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector(".navbar");
+  const hamburger = document.querySelector(".hamburger");
+  const overlay = document.querySelector(".overlay");
+
+  hamburger.addEventListener("click", function () {
+    navbar.classList.toggle("open");
+    overlay.classList.toggle("show");
+  });
+
+  overlay.addEventListener("click", function () {
+    navbar.classList.remove("open");
+    overlay.classList.remove("show");
+  });
+});
